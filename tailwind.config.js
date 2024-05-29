1265271e00025567bd90a8c56138028709a28f7b
edliau <edwardlys.work@gmail.com>
2024-05-29 15:48:30 +0800

    extending custom fonts

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        custom: ['JUSTSansRegular', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
