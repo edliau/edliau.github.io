74ef95c7def941fe2dc78c6164a084278bf7901a
edliau <edwardlys.work@gmail.com>
2024-05-29 20:52:27 +0800

    additional js and styling

--- scripts/typing.js
+++ scripts/typing.js
@@ -1,6 +1,16 @@
-function typingEffect(textContainerSelector, words) {
-    const textContainer = document.querySelector(textContainerSelector);
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
 
+function typingEffect(textContainerSelector, words) {
+    // Shuffle the words array
+    words = shuffleArray(words);
+
+    const textContainer = document.querySelector(textContainerSelector);
     let wordIndex = 0;
     let letterIndex = 0;
 
@@ -9,9 +19,9 @@ function typingEffect(textContainerSelector, words) {
         textContainer.textContent = currentWord.substring(0, letterIndex + 1);
         letterIndex++;
         if (letterIndex === currentWord.length) {
-            setTimeout(eraseWord, 1000);
+            setTimeout(eraseWord, 4000); // Increase delay before erasing
         } else {
-            setTimeout(typeNextWord, 200);
+            setTimeout(typeNextWord, 100); // Delay between each letter typing
         }
     }
 
@@ -20,15 +30,16 @@ function typingEffect(textContainerSelector, words) {
         textContainer.textContent = currentWord.substring(0, letterIndex);
         letterIndex--;
         if (letterIndex >= 0) {
-            setTimeout(eraseWord, 50);
+            setTimeout(eraseWord, 50); // Delay between each letter erasing
         } else {
             wordIndex++;
             if (wordIndex === words.length) {
                 wordIndex = 0;
             }
-            setTimeout(typeNextWord, 1000);
+            setTimeout(typeNextWord, 300);
         }
     }
 
+    // Initial start of typing effect
     typeNextWord();
 }
